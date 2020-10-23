b8248b6fe27bd032dcc4aa08f318b4a8ca3a3c41
Frans Lourens <franslourens86@gmail.com>
2020-10-23 10:25:33 +0200

    install script

--- Dockerfile.php
+++ Dockerfile.php
@@ -20,4 +20,13 @@ RUN docker-php-ext-enable memcache memcached
 RUN addgroup --gid 2000 --system wordpress
 RUN adduser --uid 2000 --system --disabled-login --disabled-password --gid 2000 wordpress
 
-RUN wget https://downloads.wordpress.org/plugin/insert-php-code-snippet.1.3.1.zip && unzip insert-php-code-snippet.1.3.1.zip && mv insert-php-code-snippet /var/www/html/wp-content/plugins/
+
+WORKDIR /usr/src/app/
+COPY wordpress/ /usr/src/app/
+
+RUN chmod +x install.sh && \
+    sh install.sh && \
+    rm -rf \
+    install.sh \
+    plugins.txt \
+    themes.txt
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+mkdir -p plugins
+for download_url in $(cat plugins.txt)
+do
+    curl -Ls $download_url -o plugin.zip
+    unzip -oq plugin.zip -d plugins/
+    rm -f plugin.zip
+done
+
+mkdir -p themes
+
+for download_url in $(cat themes.txt)
+do
+    curl -Ls $download_url -o theme.zip
+    unzip -oq theme.zip -d themes/
+    rm -f theme.zip
+done
