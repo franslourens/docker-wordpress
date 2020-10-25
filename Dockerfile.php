78b194cf44fdbe21cc84719e4b502e23716875ef
Frans Lourens <franslourens86@gmail.com>
2020-10-25 14:41:19 +0200

    plugins and themes

--- Dockerfile.php
+++ Dockerfile.php
@@ -19,3 +19,8 @@ RUN docker-php-ext-enable memcache memcached
 
 RUN addgroup --gid 2000 --system wordpress
 RUN adduser --uid 2000 --system --disabled-login --disabled-password --gid 2000 wordpress
+
+COPY insert-php-code-snippet.1.3.1.zip /usr/src/wordpress/wp-content/plugins/
+COPY theme.zip /usr/src/wordpress/wp-content/themes/
+
+RUN unzip /usr/src/wordpress/wp-content/plugins/insert-php-code-snippet.1.3.1.zip -d /usr/src/wordpress/wp-content/plugins/ && unzip /usr/src/wordpress/wp-content/themes/theme.zip -d /usr/src/wordpress/wp-content/themes/
